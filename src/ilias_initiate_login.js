20408f24bd19a5c6e2e1f34858b2a43e7c386e1a
floriensk <48457906+floriensk@users.noreply.github.com>
2022-07-20 03:04:07 +0200

    Add redirecting
    Add the option to create a URL that, after going through the same login procedure, will redirect to the target URL specified. This is useful because sometimes due to the login procedure the original target URL (i.e. the URL of a specific course) is lost and only a general ILIAS page is reached. The extension can then manually redirect to the course page.

--- src/ilias_initiate_login.js
+++ src/ilias_initiate_login.js
@@ -1,3 +1,19 @@
+async function checkRedirect() {
+    // read and remove the URL from storage to redirect to
+    const sessionStorageRedirectKey = "redirectUrl";
+    const redirectUrl = await browser.storage.local.get(sessionStorageRedirectKey)
+        .then(s => s?.redirectUrl, f => undefined);
+    await browser.storage.local.remove(sessionStorageRedirectKey);
+
+    if (redirectUrl != null) {
+        console.log("ILIAS automatic login: redirecting to redirect URL");
+
+        window.location.href = redirectUrl;
+    } else {
+        console.log("ILIAS automatic login: no redirect URL was found");
+    }
+}
+
 // Choose normal account type on the account type Ilias page
 var accountButton = document.getElementById("button_shib_login");
 if (accountButton != null) {
@@ -5,5 +21,13 @@ if (accountButton != null) {
 } else {
     // If we're not on the account type page, initiate the login on an Ilias page
     var submitButton = document.querySelector("button > span[aria-label=\"Anmelden\"]");
-    submitButton?.parentElement.click();
+
+    if (submitButton) {
+        console.log("ILIAS automatic login: initiating login");
+
+        submitButton.parentElement.click();
+    } else {
+        // If no login button is found, that suggests that we are logged in. Check if there is an url saved for redirecting to
+        checkRedirect().then(function () { });
+    }
 }
--- src/ilias_login.js
+++ src/ilias_login.js
@@ -1,4 +1,4 @@
-var submitButton = document.getElementById("sbmt");
+const submitButton = document.getElementById("sbmt");
 
 function sleep(duration) {
     return new Promise(resolve => setTimeout(resolve, duration));
@@ -7,17 +7,34 @@ function sleep(duration) {
 /// Checks if the login form is filled, and if so, submits the credentials
 function checkFormFilled() {
     var filled = document.getElementById("password").value ? true : false;
-    if (filled)
-        submitButton?.click();
+    if (filled) {
+        console.log("ILIAS automatic login: logging in");
+
+        submitButton.click();
+    }
 
     return filled;
 }
 
-// Check a few times if the form is filled by a password manager
-const sleepDuration = 300;
-sleep(sleepDuration)
-    .then(() => !checkFormFilled())
-    .then(async () => await sleep(sleepDuration))
-    .then(() => !checkFormFilled())
-    .then(async () => await sleep(sleepDuration))
-    .then(() => !checkFormFilled());
+function login() {
+    if (!submitButton) {
+        console.warn("ILIAS automatic login: login button was not found");
+        
+        return;
+    }
+    
+    // Check a few times if the form is filled by a password manager
+    const sleepDuration = 300;
+    const rejectOnceFilled = () => checkFormFilled() ? Promise.reject() : Promise.resolve();
+    
+    let task = rejectOnceFilled();
+
+    for (let i = 0; i < 4; i++) {
+        task = task
+            .then(async () => await sleep(sleepDuration))
+            .then(() => rejectOnceFilled());
+    }
+    task.then(() => browser.storage.local.remove("redirectUrl"));
+}
+
+login();
